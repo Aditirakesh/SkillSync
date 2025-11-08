a1da00dde4d86c8ee397222c0c1626b4d6cee6e1
Ishant-93 <89799396+Ishant-93@users.noreply.github.com>
2025-11-08 12:49:44 +0530

    Fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -54,7 +54,7 @@ exports.login = async (req, res) => {
 
     const token = jwt.sign(
       { id: user._id, role: user.role },
-      JWT_SECRET,
+      process.env.JWT_SECRET,
       { expiresIn: TOKEN_EXPIRY }
     );
 
